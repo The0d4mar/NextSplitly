98702bc9abf0f8d1d6efa568559dc78572897cd5
Vladimir Gornyy <112384479+The0d4mar@users.noreply.github.com>
2025-09-06 14:45:50 +0300

    refactor(page): add indicatorCont

--- nextsplitly/src/app/page.tsx
+++ nextsplitly/src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import IndicatorsCont from "@/components/shades/IndicatorsCont";
+import UserFilterPart from "@/components/shades/UserFilterPart";
 import UserFiltersBtn from "@/components/shades/UserFiltersBtn/UserFiltersBtn";
 import { RootState } from "@/store/store";
 import { MainPageContainer } from "@/styled/MainPageCont";
@@ -12,24 +14,8 @@ export default function Home() {
   const filterPeriod = ['По дням', 'По месяцам', 'По годам']
   const walletFiltres = ['Все счета', 'Основной', 'Накопительный', 'Общий']
   return (
-    <div className="flex flex-col w-352" >
-        <MainPageContainer>
-          <div className="flex-col justify-between items-start box-content whitespace-nowrap">
-            <div className="userName" style={{fontSize: SIZES.fontSizeH2, fontWeight:SIZES.fontWeightH2}}>
-              {activeUserStore.name} {activeUserStore.secondName}
-            </div>
-            <div className="userDateOfRegistration" style={{fontSize: SIZES.fontSizeSmall, color: `${COLORS.fontColor_opt}`}}>
-              Дата регистрации: {activeUserStore.registerData}
-            </div>
-            
-          </div>
-          <div className="flex justify-between items-center gap-10">
-            <UserFiltersBtn title={"Отображение"} choosenfilter={filterPeriod[0]} filters = {filterPeriod} />
-            <UserFiltersBtn title={"Счета"} choosenfilter={walletFiltres[0]} filters = {walletFiltres} />
-            
-            
-          </div>
-        </MainPageContainer>
+    <div className="flex flex-col w-352 relative" >
+        <IndicatorsCont children={<UserFilterPart activeUserStore={activeUserStore} filterPeriod={filterPeriod} walletFiltres={walletFiltres}/>}/>
     </div>
   );
 }
